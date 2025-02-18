1270df73db7f3fd8668be9f51baecdc45e675bd7
Sounish Nath <flock.sinasini@gmail.com>
2025-02-18 20:13:45 +0530

    feat(auth): add login functionality to AuthService with HTTP request handling; update docker-compose for MongoDB volume persistence

--- web/src/app/modules/auth/services/auth.service.ts
+++ web/src/app/modules/auth/services/auth.service.ts
@@ -9,6 +9,13 @@ export class AuthService {
 
   constructor(private readonly httpClient: HttpClient) { }
 
+  login$(email: string, password: string) {
+    const loginPayload = { email, password };
+    // configure headers
+    const headers = this.configureHttpHeaders();
+    return this.httpClient.post(`${environment.NG_REFERRER_BACKEND_API_URL}/api/auth/login`, loginPayload, { headers });
+  }
+
   signup$(email: string, password: string) {
     const signupPayload = { email, password };
 
