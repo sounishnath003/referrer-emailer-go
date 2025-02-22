ac0cfa33d9a78e64136e1a247409f29b995adc3b
Sounish Nath <flock.sinasini@gmail.com>
2025-02-22 21:15:30 +0530

    feat(landing): update landing page layout and add footer with credits

--- web/src/app/modules/landing/landing.component.ts
+++ web/src/app/modules/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-landing',
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
