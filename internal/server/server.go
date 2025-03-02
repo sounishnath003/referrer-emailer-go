0d7574374c5794d30801c01182452e7807996f46
Sounish Nath <flock.sinasini@gmail.com>
2025-03-02 10:01:14 +0530

    feat(docker): configure referrer-emailer service with build context and add Google credentials volume feat(app): integrate CORS interceptor for HTTP requests

--- internal/server/server.go
+++ internal/server/server.go
@@ -46,10 +46,11 @@ func (s *Server) Start() error {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:4200", "http://localhost:3000"},
-		AllowMethods: []string{echo.POST, echo.GET},
-		AllowHeaders: []string{"X-API-TrackerId", echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAuthorization, echo.HeaderContentLength},
-		MaxAge:       time.Now().Add(1 * time.Hour).Second(),
+		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowMethods:     []string{echo.POST, echo.GET},
+		AllowCredentials: true,
+		AllowHeaders:     []string{"X-API-TrackerId", echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAuthorization, echo.HeaderContentLength},
+		MaxAge:           time.Now().Add(1 * time.Hour).Second(),
 	}))
 	e.Use(echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(utils.GetStringFromEnv("JWT_SECRET_KEY", "Sec%&!*RT#*!@(89231%&!*RT#12345")),
---
+++ web/src/app/shared/interceptors/cors.interceptor.ts
@@ -0,0 +1,12 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const corsInterceptor: HttpInterceptorFn = (req, next) => {
+  const modifiedRequest = req.clone({
+    setHeaders: {
+      // "Access-Control-Allow-Origin": "http://localhost:4200",
+      "Referer": "http://localhost:4200/",
+      "Content-Type": "application/json",
+    }
+  })
+  return next(modifiedRequest);
+};
