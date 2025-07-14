7fab7bec2300c467d9f828b84cf56a50e3928d60
Sounish Nath <flock.sinasini@gmail.com>
2025-07-15 00:47:56 +0530

    WIP

--- web/src/app/modules/dashboard/pages/shared/menu/menu.component.ts
+++ web/src/app/modules/dashboard/pages/shared/menu/menu.component.ts
@@ -1,9 +1,10 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
